caacde59145b3cf74274472c9eec159b6889ef72
milamgm <nocturnetheater@gmail.com>
2023-02-15 22:42:12 +0100

    gh-pages added

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,22 @@ import { AppContextProvider } from "./context/UseStateContext";
 import CompletedOrders from "./pages/CompletedOrders";
 import DeletedOrders from "./pages/DeletedOrders";
 import Orders from "./pages/Orders";
-import "./styles/styles.css"
-
+import "./styles/styles.css";
 
 function App() {
   return (
     <div>
       <AppContextProvider>
-        <Navbar />
-        <Container fluid>
-          <Routes>
-            <Route path="/" element={<Orders />} />
-            <Route path="/completedorders" element={<CompletedOrders />} />
-            <Route path="/deletedorders" element={<DeletedOrders />} />
-          </Routes>
-        </Container>
+        <>
+          <Navbar />
+          <Container fluid>
+            <Routes>
+              <Route path="/" element={<Orders />} />
+              <Route path="/completedorders" element={<CompletedOrders />} />
+              <Route path="/deletedorders" element={<DeletedOrders />} />
+            </Routes>
+          </Container>
+        </>
       </AppContextProvider>
     </div>
   );
